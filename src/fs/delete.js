dab9cb0bb8f2be8e0afadea9992c04aea4389244
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-27 23:11:01 +0700

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = './src/fs/files/fileToRemove.txt';
+  const errorText = 'FS operation failed';
+
+  try {
+    await fs.unlink(filePath);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error(errorText);
+    } else {
+      throw err;
+    }
+  }
 };
 
 await remove();
