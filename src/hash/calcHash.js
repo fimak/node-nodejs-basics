c3ae88f319f5a26e28ee8f89502acf0824d34513
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-28 01:39:11 +0700

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const fileData = await fs.readFile(filePath, { encoding: 'utf-8' });
+  const hash = createHash('sha256');
+  hash.update(fileData);
+  const fileHash = hash.digest('hex');
+  process.stdout.write(fileHash);
 };
 
 await calculateHash();
