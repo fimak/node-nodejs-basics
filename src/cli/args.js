8b682d705f34713744f0ca31d243ee9b933447aa
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-28 00:20:37 +0700

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const args = process.argv.slice(2);
+
+  const paramsString = args.reduce((acc, el, id) => {
+    if (el.startsWith('--')) {
+      acc = `${acc ? acc + ', ' : ''}${el.slice(2)} is ${args[id + 1]}`;
+    }
+    return acc;
+  }, '');
+
+  console.log(paramsString);
 };
 
 parseArgs();
