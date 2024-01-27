afe7164b6ad2e6e3cfc28eadbf32d40d5c94e524
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-27 23:40:52 +0700

    feat: implemented read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const filesPath = './src/fs/files/fileToRead.txt';
+  const errorText = 'FS operation failed';
+
+  try {
+    const content = await fs.readFile(filesPath, { encoding: 'utf-8'});
+    console.log(content);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error(errorText);
+    } else {
+      throw err;
+    }
+  }
 };
 
 await read();
