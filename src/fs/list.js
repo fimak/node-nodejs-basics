52e9744bc33a6a836e863c0b4924cc7ba1832035
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-27 23:34:31 +0700

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises';
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  const filesPath = './src/fs/files';
+  const errorText = 'FS operation failed';
+
+  try {
+    const files = await fs.readdir(filesPath);
+    files.forEach((file) => console.log(file));
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error(errorText);
+    } else {
+      throw err;
+    }
+  }
 };
 
 await list();
