4f96dea978f190ef55b6619069b68fcdf26e99db
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-28 00:04:06 +0700

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const paramsString =  Object.keys(process.env).reduce((acc, el) => {
+    if (el.startsWith('RSS_')) {
+      acc = `${acc ? acc + '; ' : ''}${el}=${process.env[el]}`;
+    }
+    return acc;
+  }, '')
+
+  console.log(paramsString);
 };
 
 parseEnv();
