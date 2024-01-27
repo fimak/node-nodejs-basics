9cda12c21c2c7d32fbe3b5e50b53ee71329f629c
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-27 23:00:31 +0700

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  const wrongFileName = './src/fs/files/wrongFilename.txt';
+  const properFileName = './src/fs/files/properFilename.md';
+  const errorText = 'FS operation failed';
+
+  try {
+    await fs.rename(wrongFileName, properFileName);
+    await fs.access(properFileName);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error(errorText);
+    }
+    throw err;
+  }
 };
 
 await rename();
