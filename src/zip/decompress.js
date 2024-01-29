617fc2ab962b794f9661a7b9643972eb6e9ec716
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-29 23:02:14 +0700

    feat: implement decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const fileCompressedPath = path.join(__dirname, 'files', 'archive.gz');
+
+  try {
+    const readStream = fs.createReadStream(fileCompressedPath);
+    const writeStream = fs.createWriteStream(filePath);
+    const decompressStream = zlib.createGunzip();
+
+    readStream
+      .pipe(decompressStream)
+      .pipe(writeStream);
+
+  } catch (err) {
+    throw err;
+  }
 };
 
 await decompress();
