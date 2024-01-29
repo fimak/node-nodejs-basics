80bf50f5a969cd2a86589fd47c4e32566af93a0f
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-29 22:58:51 +0700

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const fileCompressedPath = path.join(__dirname, 'files', 'archive.gz');
+
+  try {
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(fileCompressedPath);
+    const compressStream = zlib.createGzip();
+
+    readStream
+      .pipe(compressStream)
+      .pipe(writeStream);
+
+  } catch (err) {
+    throw err;
+  }
 };
 
 await compress();
