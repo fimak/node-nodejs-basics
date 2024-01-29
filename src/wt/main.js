3f930aa60a333689b3337bc577db6477aadb0a89
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-30 01:46:50 +0700

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const coresAmount = os.cpus().length;
+  const results = [];
+  let completedWorkers = 0;
+
+  const workers = new Array(coresAmount).fill(null).map((el, i) => {
+    const worker = new Worker('./worker.js');
+
+    worker.on('message', (message) => {
+      results[i] = message;
+      completedWorkers++;
+      if (completedWorkers === coresAmount) {
+        console.log(results);
+      }
+    });
+
+    worker.postMessage(10 + i);
+  });
 };
 
 await performCalculations();
