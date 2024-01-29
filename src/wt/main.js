364dcdd42f4512c4767795a061f22b694153dbf0
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-30 01:55:41 +0700

    fix: use the relative path to worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,14 +1,18 @@
+import path from 'node:path';
 import { Worker } from 'node:worker_threads';
 import os from 'node:os';
 
 const performCalculations = async () => {
   // Write your code here
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = path.dirname(__filename);
+  const workerPath = path.join(__dirname, 'worker.js');
   const coresAmount = os.cpus().length;
   const results = [];
   let completedWorkers = 0;
 
   const workers = new Array(coresAmount).fill(null).map((el, i) => {
-    const worker = new Worker('./worker.js');
+    const worker = new Worker(workerPath);
 
     worker.on('message', (message) => {
       results[i] = message;
