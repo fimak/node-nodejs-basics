6c1273702a655d4961d23b5f05ab3b3eab40d8b9
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-29 21:30:14 +0700

    feat: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 const write = async () => {
-    // Write your code here 
+  // Write your code here
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+  try {
+    const fd = await fs.open(filePath, 'w');
+    const writeStream = fd.createWriteStream();
+
+    process.stdin.pipe(writeStream);
+  } catch(err) {
+    throw err;
+  }
 };
 
 await write();
