d7ccf8c199ee2af96072874b572a958d44ae30d5
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-29 20:15:47 +0700

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    const fd = await fs.open(filePath);
+    const readStream = fd.createReadStream();
+
+    readStream.on('data', (chunk) => {
+      process.stdout.write(chunk.toString());
+    });
+  } catch(err) {
+    throw err;
+  }
 };
 
 await read();
