695ecdb16009f91c251735afa9625c51bc47fb4b
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-29 21:57:56 +0700

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
 const transform = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    process.stdin
+      .on('data', (chunk) => {
+        process.stdout.write(chunk.toString().split('').reverse().join(''))
+      })
+  } catch(err) {
+    throw err;
+  }
 };
 
 await transform();
